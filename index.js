4e8d654d43fd92a9e1d3c1c4b6f12ee6c0dd85f6
Artyom <artyomcarman@gmail.com>
2020-03-25 03:03:16 +0400

    Add port for local server

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const telegramBot = require("node-telegram-bot-api");
-const TOKEN = require('./token.js');
+const TOKEN = require("./token.js");
+const http = require("http");
 
-const bot = new telegramBot(TOKEN, {polling: true});
+const PORT = process.env.PORT || 3000;
 
-bot.on('message', msg => {
-  console.log(msg)
+const server = http.createServer();
+server.listen(PORT);
+
+const bot = new telegramBot(TOKEN, { polling: true });
+
+bot.on("message", msg => {
+  console.log(msg);
   if (msg.from.id !== 533139971) {
-    bot.sendMessage(msg.chat.id, `Извини, но ты лох`)
+    bot.sendMessage(msg.chat.id, `Извини, но ты лох`);
   }
-  bot.sendMessage(msg.chat.id, `Здорова ёпта, ${msg.from.first_name}`)
-})
+  bot.sendMessage(msg.chat.id, `Здорова ёпта, ${msg.from.first_name}`);
+});
