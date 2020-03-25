c2608aeec743106f776ea78428a7840744f172be
Artyom <artyomcarman@gmail.com>
2020-03-25 12:01:17 +0400

    add NTBA_FIX_319

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const telegramBot = require("node-telegram-bot-api");
 const bot = new telegramBot(process.env.TOKEN, { polling: true });
-
+process.env.NTBA_FIX_319 = 1
 bot.on("message", msg => {
   console.log(msg);
   bot.sendMessage(msg.chat.id, `Здорова ёпта, ${msg.from.first_name}`);
