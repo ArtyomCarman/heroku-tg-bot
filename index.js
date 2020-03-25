e43d30e88886abe61c22270cbcf762cfa37dddfd
Artyom <artyomcarman@gmail.com>
2020-03-25 11:55:29 +0400

    Try to start worker

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const telegramBot = require("node-telegram-bot-api");
-const TOKEN = "1119191635:AAF9MKaGsofAVC_R1y17ObBO6jEK9Usuu6s"
-const bot = new telegramBot(TOKEN, { polling: true });
+const bot = new telegramBot(process.env.TOKEN, { polling: true });
 
 bot.on("message", msg => {
   console.log(msg);
-  if (msg.from.id !== 533139971) {
-    bot.sendMessage(msg.chat.id, `Извини, но ты лох`);
-  }
   bot.sendMessage(msg.chat.id, `Здорова ёпта, ${msg.from.first_name}`);
 });
