1ae1768f91fa447a1731dffe58ca3dc6dd7b452d
Artyom <artyomcarman@gmail.com>
2020-04-03 22:49:54 +0400

    add return func

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const TOKEN = process.env.TOKEN;
 const TelegramBot = require("node-telegram-bot-api");
 const request = require("request");
 const cheerio = require("cheerio");
-const fs = require('fs');
 
 const options = {
   webHook: {
@@ -16,7 +15,7 @@ const bot = new TelegramBot(TOKEN, options);
 
 bot.setWebHook(`${url}/bot${TOKEN}`);
 
-bot.onText("/\/start/", (msg) => {
+bot.onText("//start/", (msg) => {
   const text = `Ку, ${msg.chat.first_name}\nЧего хочешь?`;
   bot.sendMessage(msg.chat.id, text, {
     reply_markup: {
@@ -28,25 +27,23 @@ bot.onText("/\/start/", (msg) => {
 bot.on("message", (msg) => {
   switch (msg.text) {
     case "Огромный член":
-      bot.sendMessage(msg.chat.id, 'Ну, а кто не хочет?\nРиторический вопрос...')
+      bot.sendMessage(
+        msg.chat.id,
+        "Ну, а кто не хочет?\nРиторический вопрос..."
+      );
       break;
     case "расписание звонков":
-      bot.sendMessage(msg.chat.id, parsingYatt())
+      bot.sendMessage(msg.chat.id, parsingYatt());
       break;
   }
 });
 
 const parsingYatt = () => {
   const URL2 = "https://yatt.framework.team/dashboard";
-  request(URL2, (err, res, body) => {
+  return request(URL2, (err, res, body) => {
     if (err) throw err;
-      let $ = cheerio.load(body);
-      let title = $("title");
-      return title.text()
-    });
-    
-}
-
-
-
-
+    let $ = cheerio.load(body);
+    let title = $("title");
+    return title.text();
+  });
+};
